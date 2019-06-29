17e12abe733a5cf3d95bf1bca890b875f7fbeb8a
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2019-06-29 16:54:50 -0300

    Adjusting ex7

--- ex7.py
+++ ex7.py
@@ -10,32 +10,113 @@
 import numpy as np
 from PIL import Image, ImageDraw
 import MyLib as ml
-from copy import deepcopy
-import matplotlib.pyplot as plt
+
+
+def getPoints(img):
+    vertices = [(i, j) for i in range(img.shape[0]) for j in range(img.shape[1]) if img[i, j] == 255]
+    vertices = np.array(vertices)
+    return vertices
+
+
+def makeGrid(img, gridSize):
+    grid = [(i, j) for i in range(0, img.shape[0], gridSize) for j in range(0, img.shape[1], gridSize)]
+    return grid
+
+
+def convert2grid(vertices, grid):
+    points = []
+    grid = np.array(grid)
+    for v in vertices:
+        d = np.sqrt(np.square(grid - v).sum(axis=1))
+        points.append(grid[np.argmin(d)])
+
+    points = np.array(points)
+    points = np.unique(points, axis=0)
+    points = [(j, i) for i, j in points]
+    return points
+
+
+def getStartPoint(points):
+    max, idx = 0, 0
+    for i, (y, x) in enumerate(points):
+        if y > max:
+            max = y
+            idx = i
+
+    return points[idx], idx
+
+
+def getNextPoint(currentPoint, freemanIdx, gridSize):
+    if freemanIdx == 0:
+        return currentPoint[0] + gridSize, currentPoint[1]
+    elif freemanIdx == 1:
+        return currentPoint[0] + gridSize, currentPoint[1] + gridSize
+    elif freemanIdx == 2:
+        return currentPoint[0], currentPoint[1] + gridSize
+    elif freemanIdx == 3:
+        return currentPoint[0] - gridSize, currentPoint[1] + gridSize
+    elif freemanIdx == 4:
+        return currentPoint[0] - gridSize, currentPoint[1]
+    elif freemanIdx == 5:
+        return currentPoint[0] - gridSize, currentPoint[1] - gridSize
+    elif freemanIdx == 6:
+        return currentPoint[0], currentPoint[1] - gridSize
+    elif freemanIdx == 7:
+        return currentPoint[0] + gridSize, currentPoint[1] - gridSize
+
+
+def getFreemanChain(points, gridSize):
+    startPoint, idx = getStartPoint(points)
+
+    freemanChain = []
+
+    currentPoint = startPoint
+    sortedPoints = []
+    # points.append(startPoint)
+
+    while len(points) != 1:
+        for i in range(8):
+            nextPoint = getNextPoint(currentPoint, i, gridSize)
+            if nextPoint in points:
+                freemanChain.append(i)
+                points.remove(currentPoint)
+                sortedPoints.append(currentPoint)
+                currentPoint = nextPoint
+                break
+
+    sortedPoints.append(currentPoint)
+    sortedPoints.append(startPoint)
+    return freemanChain, sortedPoints
+
 
 img = Image.open('images/Fig11.10.jpg')
 img = np.array(img)
 
+# Getting object border
 border = ml.conv2D(img, ml.LAPLACE_FILTER)
+
 Image.fromarray(border).show()
-vertices = [(i, j) for i in range(img.shape[0]) for j in range(img.shape[1]) if border[i, j] == 255]
-vertices = np.array(vertices)
 
-gridSize = 40
-grid = [(i, j) for i in range(0, img.shape[0]+1, gridSize) for j in range(0, img.shape[1]+1, gridSize)]
-grid = np.array(grid)
+# Getting border points
+vertices = getPoints(border)
 
-points = []
-for v in vertices:
-    d = np.sqrt(np.square(grid - v).sum(axis=1))
-    points.append(grid[np.argmin(d)])
+gridSize = 43
+# Creating grid
+grid = makeGrid(border, gridSize)
 
-points = np.array(points)
-points = np.unique(points, axis=0)
-points = [(j, i) for i, j in points]
+# Resampling border points to grid
+points = convert2grid(vertices, grid)
+
+# Finding Freeman chain
+f, p = getFreemanChain(points, gridSize)
+
 result = Image.new('L', (img.shape[1], img.shape[0]))
-d = ImageDraw.Draw(result)
-print(len(points))
-d.point(points, fill=255)
-
-result.show()
+border = Image.fromarray(border)
+d = ImageDraw.Draw(border)
+grid = [(j, i) for i, j in grid]
+d.line(p, fill=255)
+d.point(grid, fill=0)
+print("Grid size: {}".format(gridSize))
+print("Freeman chain size: {}".format(len(f)))
+print("Freeman chain: {}".format(f))
+border.show()
